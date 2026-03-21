b7cc5a2f457739b522df007b103720318995d5a8
Drigo019 <rcarvalho15022009@gmail.com>
2026-03-21 11:42:27 -0300

    Update script.js

--- Tela Inicial/script.js
+++ Tela Inicial/script.js
@@ -1,6 +1,18 @@
 function menu()
     {
-        alert("Menu");
+// Obter referências dos elementos
+        var select = document.getElementById("pagamento");
+        var tabela = document.getElementById("minhaTabela");
+
+// 4. Lógica JavaScript para mostrar/esconder
+        if (select.value === "Dinheiro") 
+            {
+                tabela.style.display = "table"; // Exibe a tabela
+            } 
+        else 
+            {
+                tabela.style.display = "none"; // Oculta a tabela
+            }
     }
 function novoPedido()
     {
@@ -19,3 +31,6 @@ function finalizarPedido()
         var formadepagamento = document.getElementById("pagamento").value;
         alert("Forma de pagamento selecionada: " + formadepagamento);
     }
+function mostrarTabela() {
+           
+        }
