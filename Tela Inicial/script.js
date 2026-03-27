d60ee2ed535186d16caa31c446a4e2e2870b4225
Drigo019 <rcarvalho15022009@gmail.com>
2026-03-27 10:49:35 -0300

    Update script.js

--- Tela Inicial/script.js
+++ Tela Inicial/script.js
@@ -19,7 +19,7 @@ function finalizarPedido()
         var formadepagamento = document.getElementById("pagamento").value;
         alert("Forma de pagamento selecionada: " + formadepagamento);
     }
-function ClienteSemCadastro() 
+function Cadastrar() 
     {
         alert("Cliente não Cadastrado:");
     }
