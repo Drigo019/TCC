d980c38b5309a4fd3c2de02610e2469b4ef2517e
Drigo019 <rcarvalho15022009@gmail.com>
2026-04-03 12:46:05 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,15 +6,3 @@
     $porta = '3306';
     $conexao = mysqli_connect($servidor, $usuario, $senha, $banco, $porta);
 
-    global $pdo;
-
-    try 
-        {
-            $pdo = new PDO($servidor, $usuario, $senha);
-            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }
-    catch (PDOException $erro)
-        {
-            return ''. $erro->getMessage() .'';
-            exit;
-        } 
