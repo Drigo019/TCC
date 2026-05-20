acb0a6e7450651cef78d467c60050f66d8c550ee
Drigo019 <rcarvalho15022009@gmail.com>
2026-05-20 11:36:07 -0300

    20/5/2026 11:36

--- TelaInicial.php
+++ TelaInicial.php
@@ -46,9 +46,9 @@ session_start();
     <div id="centro" style="height: auto; min-height: 100px;">
         <div class="slider">
             <div class="slides"> <!--? Onde estarão as imagens -->
-                <img src="../TCC/fotos_queijos/queijo1.jpeg" alt="imagem 1" class="slide active" style="width: 900px;">
-                <img src="../TCC/fotos_queijos/queijo2.jpeg" alt="imagem 2" class="slide"        style="width: 900px;">
-                <img src="../TCC/fotos_queijos/queijo3.jpeg" alt="imagem 3" class="slide"        style="width: 900px;">
+                <img src="../TCC/fotos_queijos/queijo1.jpeg" alt="imagem 1" class="slide active accordion reajuste" style="width: 900px;">
+                <img src="../TCC/fotos_queijos/queijo2.jpeg" alt="imagem 2" class="slide reajuste"        style="width: 900px;">
+                <img src="../TCC/fotos_queijos/queijo3.jpeg" alt="imagem 3" class="slide reajuste"        style="width: 900px;">
             </div>
             
             <div class="indicators"> <!--? seletores-->
