7d6b567fe8aaa69c44d5b65f1631d91eb55423bd
LeonardoL14 <leonardoluis1427@gmail.com>
2026-06-17 11:11:35 -0300

    1

--- SalvarImagem.php
+++ SalvarImagem.php
@@ -1,21 +1,4 @@
 <?php
-include 'conexao.php';
-
-$nome = $_POST['nome'];
-$preco = $_POST['preco'];
-$estoque = $_POST['estoque'];
-$codigo = $_POST['codigo_barras'];
-
-$sql = "INSERT INTO produtos
-(nome, preco, estoque, codigo_barras)
-VALUES
-('$nome', '$preco', '$estoque', '$codigo')";
-
-$conn->query($sql);
-
-echo "Produto cadastrado com sucesso!";
-?>
-<?php
 $pdo = new PDO('mysql:host=localhost;dbname=containerdoqueijo', 'root', '');
 
 if(isset($_FILES['arquivo'])){
