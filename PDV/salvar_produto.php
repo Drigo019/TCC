5d03592a8d8336cc7ca22d9c2ca9fa38f85f95e0
LeonardoL14 <leonardoluis1427@gmail.com>
2026-06-17 11:31:48 -0300

    17/6/2026 11:31
    Mudanças: salvar a imagem do produto em uma pasta separada

--- PDV/salvar_produto.php
+++ PDV/salvar_produto.php
@@ -29,11 +29,11 @@ if(isset($_FILES['arquivo'])){
     //Move a imagem da pasta temporário para a definitiva 
     if(move_uploaded_file($arquivo['tmp_name'], $caminhoSalvar)){
         //Insere o caminho da imagem no banco de dados 
-        $stmt = $pdo-prepare("INSERT INTO imagens (caminho) VALUE (:caminho)");
+        $stmt = $pdo->prepare("INSERT INTO imagens (caminho) VALUE (:caminho)");
         $stmt->bindParam(':caminho', $caminhoSalvar);
         $stmt->execute();
 
-        echo "Imagem enviada e salva com sucesso";
+        echo "Imagem enviada e salva com sucesso"; 
     } else {
         echo"Erro as salvar a imagem";
     }
